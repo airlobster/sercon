eae299bfbff9ecb73dd8d6af049128cfdbf8b712
airlobster <adid172@gmail.com>
2026-07-01 20:24:57 +0300

    sercon

--- src/shell.h
+++ src/shell.h
@@ -1,6 +1,13 @@
 #ifndef _SHELL_H
 #define _SHELL_H
 
+/**
+ * @brief Execute a shell command.
+ * @param argv The argument vector for the command.
+ * @param input Optional input to pass to the command's stdin.
+ * @return int The exit status of the command.
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
