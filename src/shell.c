870a98e39e9c06566a93b62210699a61ca95ff2f
airlobster <adid172@gmail.com>
2026-07-01 19:35:04 +0300

    sercon

--- src/shell.c
+++ src/shell.c
@@ -7,7 +7,12 @@
 #include "shell.h"
 #include "ansi.h"
 
-
+/**
+ * @brief Execute a shell command.
+ * @param argv The argument vector for the command.
+ * @param input Optional input to pass to the command's stdin.
+ * @return int The exit status of the command.
+ */
 int shell(const char* argv[], const char* input) {
 	int pStdin[2], pStdout[2], pStderr[2];
 	char buffer[128];
