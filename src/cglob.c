eee0c6cf5b90881e4b53fa7d29ec7e52e20357a6
airlobster <adid172@gmail.com>
2026-07-05 06:16:26 +0300

    iterator

--- src/cglob.c
+++ src/cglob.c
@@ -13,27 +13,33 @@ typedef struct {
 } cglob_options_t;
 
 typedef struct {
+	cglob_options_t* options;
 	glob_t glob_result;
 	size_t i;
 } cglob_state_t;
 
-static void* cglob_next(void** state, int* done, void* context) {
+static void cglob_free(void* state) {
+	if( ! state ) return;
+	globfree(&((cglob_state_t*)state)->glob_result);
+	free(((cglob_state_t*)state)->options);
+	free(state);
+}
 
+static void* cglob_next(void** state, int* done, void* context) {
 	if( ! state || ! done ) return NULL;
 
-	cglob_options_t* opt = (cglob_options_t*)context;
 	cglob_state_t* ctx = (cglob_state_t*)*state;
 
 	// first time - initialize state
 	if( ! *state ) {
 		ctx = malloc(sizeof(cglob_state_t));
-		memset(&ctx->glob_result, 0, sizeof(glob_t));
 		ctx->i = 0;
-		int ret = glob(opt->pattern, GLOB_TILDE | GLOB_NOSORT, NULL, &ctx->glob_result);
+		ctx->options = (cglob_options_t*)context;
+		int ret = glob(ctx->options->pattern, GLOB_TILDE | GLOB_NOSORT, NULL, &ctx->glob_result);
 		if( ret != 0 && ret != GLOB_NOMATCH ) {
-			DEBUG_MSG("Error occurred while globbing pattern: %s", opt->pattern);
+			DEBUG_MSG("Error occurred while globbing pattern: %s", ctx->options->pattern);
 			*done = 1;
-			free(ctx);
+			cglob_free(ctx);
 			return NULL;
 		}
 		*state = ctx;
@@ -47,28 +53,21 @@ static void* cglob_next(void** state, int* done, void* context) {
 			continue;
 		}
 		bool pass = false;
-		pass |= (opt->options & CGLOB_FILE_REGULAR) && S_ISREG(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_DIRECTORY) && S_ISDIR(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_SYMLINK) && S_ISLNK(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_CHAR_DEVICE) && S_ISCHR(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_BLOCK_DEVICE) && S_ISBLK(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_FIFO) && S_ISFIFO(st.st_mode);
-		pass |= (opt->options & CGLOB_FILE_SOCKET) && S_ISSOCK(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_REGULAR) && S_ISREG(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_DIRECTORY) && S_ISDIR(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_SYMLINK) && S_ISLNK(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_CHAR_DEVICE) && S_ISCHR(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_BLOCK_DEVICE) && S_ISBLK(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_FIFO) && S_ISFIFO(st.st_mode);
+		pass |= (ctx->options->options & CGLOB_FILE_SOCKET) && S_ISSOCK(st.st_mode);
 		if( pass ) {
 			return (void*)path;
 		}
 	}
 
 	*done = 1;
-	free(opt);
 
 	return NULL;
-}
-
-static void cglob_free(void* state) {
-	if( ! state ) return;
-	globfree(&((cglob_state_t*)state)->glob_result);
-	free(state);
 }
 
 iterator_t cglob_iterator(const char* pattern, unsigned long options) {
